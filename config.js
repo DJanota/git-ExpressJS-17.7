47ef0a045c916e9cfedf66e68afab3158594a407
Dawid Janota <6dawid.janota@wp.pl>
2018-03-28 08:52:02 +0200

    17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.experts = {
+    'GOOGLE_CLIENT_ID': '690840520138-2s7sl1gsvhjp6ullih0bbvbchbj40qk4.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'QhW4nvhOUGFjAn8dmk-M6xaA',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
